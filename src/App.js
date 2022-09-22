8dc26d31aedc9d898c5ca333aa0491a338487106
Afrizal Fathan <rieslinger8@gmail.com>
2022-09-22 13:04:45 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect, useState } from "react"
+
+const URL = "https://www.omdbapi.com/?s=narnia&plot=full&apikey=691f9d40"
+const reqData = () => fetch(URL).then((res) => res.json())
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [avengers, setAvengers] = useState([])
+    const [film, setFilm] = useState("")
+
+    useEffect(() => {
+        const fetchMovie = () => {
+            reqData()
+                .then((res) => {
+                    setAvengers(res.Search)
+                })
+                .catch((err) => console.log(err))
+        }
+        fetchMovie()
+    }, [])
+
+    console.log(avengers)
+
+    function buttonSearch(e) {
+        e.preventDefault()
+        console.log(film)
+    }
+
+    return (
+        <div className="App">
+            <h1>Hello World!</h1>
+            <form onSubmit={buttonSearch}>
+                <label>Masukan Movie</label>
+                <input
+                    type="text"
+                    placeholder="film..."
+                    onChange={(e) => setFilm(e.target.value)}
+                ></input>
+                <button type="submit">Submit</button>
+            </form>
+            {avengers.map((movies) => (
+                <>
+                    <li>{movies.Title}</li>
+                    <li>{movies.Plot}</li>
+                </>
+            ))}
+        </div>
+    )
 }
 
-export default App;
+export default App
